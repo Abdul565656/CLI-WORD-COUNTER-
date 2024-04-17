484c16749d98cb443f202efeab526ae4e086abec
abdul565656 <abdullahkashif12b3@gmail.com>
2024-04-17 23:32:59 +0500

    work complete

---
+++ main.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+//declare a constant 'answers' and assign it to the reult of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "ENTER YOUR WORDS TO COUNT THE WORDS: "
+    }
+]);
+const words = answers.sentence.trim().split("");
+//print the array of words to the console
+console.log(words);
+//print the word count of the sentence  to the console
+console.log(`your sentence word count is ${words.length}`);
